330834237e5f18c2c4553b4b29123726e31d663e
Tewari, Ashish <atewari@levi.com>
2018-07-14 01:12:03 +0530

    updated aside component spacing

--- components/BlogItem/BlogItemLeftAside.vue
+++ components/BlogItem/BlogItemLeftAside.vue
@@ -6,7 +6,7 @@
                     <img :src="content.image" alt="" class="img-responsive">
                 </a>
             </div>
-            <div class="col-xs-12 col-md-5 zf-mt-xs-20 zf-mt-md-0">
+            <div class="col-xs-12 col-md-5 zf-mtb-xs-20 zf-mtb-md-0">
                 <div class="zf-display-flex zf-align-items-xs-center zf-min-hei-100">
                     <div class="text-left zf-plr-xs-50">
                         <h3 class="zf-fs-head-24" v-html="content.head"></h3>
--- components/BlogItem/BlogItemRightAside.vue
+++ components/BlogItem/BlogItemRightAside.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="zf-container zf-mb-xs-40 zf-mb-md-60">
-        <div class="row zf-display-flex zf-justify-content-xs-center zf-flex-xs-column zf-flex-md-row">
-            <div class="col-xs-12 col-md-5 zf-mt-xs-20 zf-mt-md-0">
+        <div class="row zf-display-flex zf-justify-content-xs-center zf-flex-xs-column-reverse zf-flex-md-row">
+            <div class="col-xs-12 col-md-5 zf-mtb-xs-20 zf-mtb-md-0">
                 <div class="zf-display-flex zf-align-items-xs-center zf-min-hei-100">
                     <div class="text-left zf-plr-xs-50">
                         <h3 class="zf-fs-head-24" v-html="content.head"></h3>
